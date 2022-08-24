66ef7d8d982691e80b8ebbc17812d7d08c1314ea
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-08-24 20:31:38 -0300

    chore: update bash script

--- cicd.sh
+++ cicd.sh
@@ -4,8 +4,8 @@ set -e
 cd native
 rustup target add aarch64-apple-ios
 cargo build --target aarch64-apple-ios --release --lib
-# rustup target add x86_64-apple-ios
-# cargo build --target x86_64-apple-ios --release --lib
+rustup target add x86_64-apple-ios
+cargo build --target x86_64-apple-ios --release --lib
 # rustup target add aarch64-apple-ios-sim
 # cargo build --target aarch64-apple-ios-sim --release --lib
 # rustup target add x86_64-apple-ios-macabi
